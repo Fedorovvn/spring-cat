daa4c0138b57091bee18776f93fb61de5d74a44a
Fedorovvn <89463822+Fedorovvn@users.noreply.github.com>
2021-10-20 15:06:01 +0300

    Выполенное задание на проверку

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -4,9 +4,22 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
-                new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+                new AnnotationConfigApplicationContext(AppConfig.class); //Указываем спрингу где искать анотации
+
+        HelloWorld firstHelloBean = (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(firstHelloBean.getMessage());
+
+        HelloWorld anotherHelloBean = (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(anotherHelloBean.getMessage());
+
+        System.out.println(firstHelloBean.equals(anotherHelloBean));
+
+        Cat firstCatBean = (Cat) applicationContext.getBean("cat");
+        System.out.println(firstCatBean.getMessage());
+
+        Cat anotherCatBean = (Cat) applicationContext.getBean("cat");
+        System.out.println(anotherCatBean.getMessage());
+
+        System.out.println(firstCatBean.equals(anotherCatBean));
     }
 }
